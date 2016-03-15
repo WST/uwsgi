28dd8946a2b0ef9f6f7fe9c8baaa8e502049d25e
WST <ilya.averkov@gmail.com>
2016-03-15 20:45:34 +0300

    OTS changes

--- ots.sh
+++ ots.sh
@@ -2,4 +2,19 @@
 
 PYTHON="/usr/bin/python3"
 
+$PYTHON uwsgiconfig.py --clean
 $PYTHON uwsgiconfig.py --build ots
+
+mkdir out
+mkdir -p out/usr/bin
+mkdir -p out/usr/lib/uwsgi
+mkdir -p out/DEBIAN
+
+mv control out/DEBIAN
+mv uwsgi out/usr/bin
+mv *.so out/usr/lib/uwsgi
+
+fakeroot dpkg-deb --build out
+
+rm -rf out
+mv out.deb uwsgi-2.0.12.deb
